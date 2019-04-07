934b72f43286b031d3efad4140bd45db5cf641e5
WilmerCastro <wilmer19427@gmail.com>
2019-04-07 01:01:06 -0400

    agrege el modulo de mantenimiento

---
+++ GRH/Gestion de Recursos Humanos/Gestion de Recursos Humanos/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Gestion_de_Recursos_Humanos
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GRH/Gestion de Recursos Humanos/Gestion de Recursos Humanos/Controllers/departamentosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gestion_de_Recursos_Humanos.Models;
+
+namespace Gestion_de_Recursos_Humanos.Controllers
+{
+    public class departamentosController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: departamentos
+        public ActionResult Index()
+        {
+            return View(db.departamentosSet.ToList());
+        }
+
+        // GET: departamentos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            departamentos departamentos = db.departamentosSet.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // GET: departamentos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: departamentos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,codigodepartamento,nombre")] departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.departamentosSet.Add(departamentos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamentos);
+        }
+
+        // GET: departamentos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            departamentos departamentos = db.departamentosSet.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: departamentos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,codigodepartamento,nombre")] departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamentos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamentos);
+        }
+
+        // GET: departamentos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            departamentos departamentos = db.departamentosSet.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            departamentos departamentos = db.departamentosSet.Find(id);
+            db.departamentosSet.Remove(departamentos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
